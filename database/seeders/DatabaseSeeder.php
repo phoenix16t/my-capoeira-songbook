f72fc1c5da183e4766da0131ba45817dcd6b699f
Eddie J <ej@Eddies-MacBook-Pro.local>
2025-09-27 01:14:04 -0400

    added seeder and lots of songs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call(SongSeeder::class);
+        $this->call(SongsSeeder::class);
     }
 }
---
+++ database/seeders/SongsSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\DB;
+
+class SongsSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('songs')->truncate();
+        DB::table('song_titles')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        $jsonFile = database_path('seeders/songs.json');
+        $songsData = json_decode(File::get($jsonFile), true);
+
+        foreach ($songsData as $songData) {
+            $lyricsString = is_array($songData['lyrics']) ? implode("\n", $songData['lyrics']) : $songData['lyrics'];
+            $translationString = is_array($songData['translation']) ? implode("\n", $songData['translation']) : $songData['translation'];
+
+            $songId = DB::table('songs')->insertGetId([
+                'translation' => $translationString,
+                'lyrics' => $lyricsString,
+                'group_id' => $songData['group'],
+                'count' => $songData['count'],
+                'type_id' => $songData['type'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+
+            foreach ($songData['titles'] as $title) {
+                DB::table('song_titles')->insert([
+                    'song_id' => $songId,
+                    'title' => $title,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+
+            foreach ($songData['links'] as $link) {
+                DB::table('song_links')->insert([
+                    'song_id' => $songId,
+                    'url' => $link['url'],
+                    'start_time' => $link['start_time'] ?? null,
+                    'end_time' => $link['end_time'] ?? null,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
+    }
+}
