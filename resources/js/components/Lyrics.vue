d8c34f8fa113e7048c6af00b532419438d4505aa
Eddie J <ej@Eddies-MacBook-Pro.local>
2025-10-06 01:34:59 -0400

    updating lyrics

--- resources/js/components/Lyrics.vue
+++ resources/js/components/Lyrics.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="flex flex-col gap-8">
+    <div class="flex flex-col gap-4">
         <h1 v-if="shouldHighlightTitle" class="text-3xl">
             {{ song.titles[0]?.title }}
         </h1>
-        <h2 v-else class="text-2xl">{{ song.titles[0]?.title }}</h2>
+        <Link v-else :href="route('songs.show', song.id)">
+            <h2 class="text-2xl hover:underline">
+                {{ song.titles[0]?.title }}
+            </h2>
+        </Link>
 
         <div>
             <div
@@ -18,6 +22,8 @@
 
 <script setup lang="ts">
 import type { Song } from "@/types";
+import { Link } from "@inertiajs/vue3";
+import { route } from "ziggy-js";
 
 interface Props {
     shouldHighlightTitle?: boolean;
