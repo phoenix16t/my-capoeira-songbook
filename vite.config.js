181b34c3cd205a12c03237baaddd39fdd8247e97
Eddie J <ej@Eddies-MacBook-Pro.local>
2025-10-08 23:26:00 -0400

    adding hmr

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,9 @@ export default defineConfig({
     server: {
         host: "localhost",
         port: 5173,
+        hmr: {
+            host: "192.168.1.4",
+        },
     },
     resolve: {
         alias: {
