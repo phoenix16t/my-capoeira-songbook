85920ae9b3bd627827287aab222846d9b2e51a33
Eddie J <ej@Eddies-MacBook-Pro.local>
2025-11-06 00:24:45 -0500

    adding note for current songbook

--- resources/js/components/SongbookList.vue
+++ resources/js/components/SongbookList.vue
@@ -4,19 +4,31 @@
             <CheckIcon class="size-5 text-green-500" />
             Belongs to Songbooks
         </h3>
-        <ul v-if="song.songbooks?.length">
+        <ul v-if="song.songbooks?.length" class="flex flex-col gap-2">
             <li
                 v-for="songbook in song.songbooks?.sort((a, b) => a.id - b.id)"
-                class="m-2 flex cursor-pointer rounded-lg border px-4 py-2 hover:shadow-lg"
+                class="flex cursor-pointer rounded-lg border px-4 py-2 transition-all hover:shadow-lg"
                 @click="removeFromSongbook(songbook)"
             >
-                <component
-                    v-if="songbook.icon"
-                    :is="Icons[songbook.icon]"
-                    class="size-5 w-8"
-                    :color="songbook.color"
-                />
-                {{ songbook.title }}
+                <div class="flex items-center">
+                    <component
+                        v-if="songbook.icon"
+                        :is="Icons[songbook.icon]"
+                        class="size-5 w-8"
+                        :color="songbook.color"
+                    />
+
+                    <div class="flex flex-col">
+                        {{ songbook.title }}
+
+                        <div
+                            v-if="songbook.id === Number(route().params.id)"
+                            class="text-xs"
+                        >
+                            Current songbook
+                        </div>
+                    </div>
+                </div>
             </li>
         </ul>
         <div v-else>No songbooks</div>
@@ -27,12 +39,12 @@
             <XIcon class="size-5 text-red-500" />
             Missing from Songbooks
         </h3>
-        <ul v-if="songbooksWithoutSong.length">
+        <ul v-if="songbooksWithoutSong.length" class="flex flex-col gap-2">
             <li
                 v-for="songbook in songbooksWithoutSong.sort(
                     (a: Songbook, b: Songbook) => a.id - b.id,
                 )"
-                class="m-2 flex cursor-pointer rounded-lg border px-4 py-2 hover:shadow-lg"
+                class="flex cursor-pointer rounded-lg border px-4 py-2 transition-all hover:shadow-lg"
                 @click="addToSongbook(songbook)"
             >
                 <component
